24ba254256a33a3595ddb838b6244e6566ef8f22
MG18049FRA <fachmiramadiabdulah18049fra@gmail.com>
2021-12-10 08:39:59 +0700

    v12

---
+++ shop/donasi.js
@@ -0,0 +1,18 @@
+const donasibot = () => {
+return `DONASI BISA VIA E-MONEY
+
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0857 8001 3253
+2. OVO
+	ㅁ 0857 8001 3253
+3. DANA
+	ㅁ 0857 8001 3253
+	
+Semuanya atas nama Fachmi Ramadi Abdulah
+`
+	}
+
+exports.donasibot = donasibot
+ 
